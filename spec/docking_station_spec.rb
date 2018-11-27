aabbffa1e63bc186c4a315374d0501a78a56ef7c
Simon Cass <scass1991@gmail.com>
2018-11-27 15:25:50 +0000

    initial commit

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+describe DockingStation do
+end
+
+describe DockingStation do
+  describe 'It responds to release_bike' do  # Origianl one line method deemed uninformative of test
+   it { is_expected.to respond_to (:release_bike) }
+  end
+end
