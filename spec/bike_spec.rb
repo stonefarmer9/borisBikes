20775a6c228e69a47150ffd2a97ddf8f5ba82530
Simon Cass <scass1991@gmail.com>
2018-11-27 15:40:31 +0000

    Add bike class and working method

---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require 'bike'
+describe Bike do
+end
+
+describe Bike do
+  describe 'It responds to working?' do  # Origianl one line method deemed uninformative of test
+   it { is_expected.to respond_to (:working?) }
+  end
+end
